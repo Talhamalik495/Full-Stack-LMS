a3e25b7240d4f34ebc14a164b4c4907c53823ea5
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-11-01 04:35:43 +0500

    update

--- src/app/(admin)/admin/batches/page.js
+++ src/app/(admin)/admin/batches/page.js
@@ -1,8 +1,12 @@
 import BatchesTable from "../../../../components/DataTables/BatchesTable";
+// import BatchModal from "../../../../components/Modals/BatchModal";
 export default function Batches() {
   return (
-    <div className="px-10 w-full">
-      <BatchesTable />
+    <div className="w-full">
+      <div>{/* <BatchModal /> */}</div>
+      <div className="px-10 w-full">
+        <BatchesTable />
+      </div>
     </div>
   );
 }
