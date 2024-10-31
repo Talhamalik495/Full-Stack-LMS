613bdee2c946d1866bbd772ee1eedfa137b1d2a5
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-11-01 03:48:42 +0500

    update

--- src/app/(admin)/admin/dashboard/page.js
+++ src/app/(admin)/admin/dashboard/page.js
@@ -1,7 +1,7 @@
 export default function Dashboard() {
   return (
-    <div className="w-full">
-      <h1>Dashboard</h1>
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-5xl font-bold">Admin</h1>
     </div>
   );
 }
