d12052a7b22cede4cfe2316307f232bd3dc47e21
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-11-04 20:24:41 +0500

    add bcrypt

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,5 +1,22 @@
 import { dbConnect } from "@/lib/dbConnect";
+import { userModal } from "@/lib/modals/UserModal";
 
-export async function POST() {
+export async function POST(request) {
   dbConnect();
+  const data = request.json();
+  //   Check User
+  let checkUser = userModal.findOne({
+    email: data.email,
+  });
+  if (checkUser) {
+    return Response.json(
+      { error: true, meassage: "User already exsist" },
+      {
+        status: 404,
+      }
+    );
+  }
+  //   hashed password Convert
+  const saltRound = 10;
+  //   let hashedPassword=
 }
