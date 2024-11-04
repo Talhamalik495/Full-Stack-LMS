058fa22672a91ebab3fe00419773af83928e80d9
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-11-04 23:40:14 +0500

    updatae

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,22 +1,25 @@
-import { dbConnect } from "@/lib/dbConnect";
-import { userModal } from "@/lib/modals/UserModal";
+// import { dbConnect } from "@/lib/dbConnect";
+// import { userModal } from "@/lib/modals/UserModal";
 
-export async function POST(request) {
-  dbConnect();
-  const data = request.json();
-  //   Check User
-  let checkUser = userModal.findOne({
-    email: data.email,
-  });
-  if (checkUser) {
-    return Response.json(
-      { error: true, meassage: "User already exsist" },
-      {
-        status: 404,
-      }
-    );
-  }
-  //   hashed password Convert
-  const saltRound = 10;
-  //   let hashedPassword=
-}
+// export async function POST(request) {
+//   dbConnect();
+//   const data = request.json();
+//   //   Check User
+//   let checkUser = userModal.findOne({
+//     email: data.email,
+//   });
+//   if (checkUser) {
+//     return Response.json(
+//       { error: true, meassage: "User already exsist" },
+//       {
+//         status: 404,
+//       }
+//     );
+//   }
+//   //   hashed password Convert
+//   const saltRound = 10;
+//   let hashedPassword = bcrypt.hash(data.password, saltRound);
+//   data.password = hashedPassword;
+//   let newUser = new userModal({ ...data });
+//   newUser.save();
+// }
